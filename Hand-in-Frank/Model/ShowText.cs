4ed8eba5f830ee0bce786c125f53d284d64700a3
ayham <ayhamnth@hotmail.com>
2019-09-24 11:58:40 +0200

    Add project files.

---
+++ Hand-in-Frank/Model/ShowText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hand_in_Frank.Model
+{
+    public class ShowText
+    {
+        public string MyText { get; set; }
+    }
+}
---
+++ Hand-in-Frank/View/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Hand_in_Frank.Model;
+
+namespace Hand_in_Frank
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<ShowText> list = new List<ShowText>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            DialogResult result = FBD.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                listview1.Items.Clear();
+                string[] files = Directory.GetFiles(FBD.SelectedPath);
+                string[] dirs = Directory.GetDirectories(FBD.SelectedPath);
+
+                foreach (string file in files)
+                {
+                    listview1.Items.Add(file);
+                    list.Add(new ShowText() { MyText = file });
+
+                }
+
+                foreach (string dir in dirs)
+                {
+                    listview1.Items.Add(dir);
+                    list.Add(new ShowText() { MyText = dir });
+                }
+            }
+        }
+
+ 
+    }
+}
